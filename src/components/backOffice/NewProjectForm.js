d70249a2ebebefa56431880536a988f9fed53dfb
Céline Cottier <cottier.celine@gmail.com>
2020-07-22 20:47:59 +0200

    list of projects in admin part, button to add a project and buttons to delete aproject

---
+++ src/components/backOffice/NewProjectForm.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+//import "./Footer.css";
+
+function NewProjectForm({ setShowNewProjectModal, ShowNewProjectModal }) {
+
+    const [data, setData] = useState({
+        name: 'non renseigné',
+        techno: 'non renseigné',
+        url_pict: 'non renseigné',
+        url_app: 'non renseigné',
+        description: 'non renseigné'
+      })
+
+
+  const handleSubmit = (e) => {
+      console.log("formulaire soumis")
+      e.preventDefault()
+    axios
+      .post('http://localhost:8080/api/projects', data)
+      .then((res) => res.data)
+      .then((res) => alert(`${res}`))
+      .catch((err) => alert(`erreur : ${err.response.data} `))
+  }
+
+  const handleChange = (e) => {
+      console.log('form changed')
+      const { name, value } = e.target
+      setData({ ...data, [name]: value })
+  }
+
+  return (
+    <div>
+    <button onClick={() => setShowNewProjectModal(false)}>Retour à la liste des projets</button>
+    <form className='form' onSubmit={handleSubmit}>
+    
+    <h2>Ajouter un nouveau projet</h2>
+    <hr className='hr-col' />
+    <div className='form-container'>
+        <div className='divForm'>
+          <label htmlFor='name'>Nom du projet</label>
+          <input type='text' name='name' id='name' className='form-input' onChange={handleChange} />
+        </div>
+        <div className='divForm'>
+          <label htmlFor='techno'>technologies utilisées</label>
+          <input type='text' name='techno' id='techno' className='form-input' onChange={handleChange} />
+        </div>
+        <div className='divForm'>
+          <label htmlFor='url_pict'>lien de la Photo</label>
+          <input type='text' name='url_pict' id='url_pict' className='form-input' onChange={handleChange} />
+        </div>
+        <div className='divForm'>
+          <label htmlFor='url_app'>lien du site</label>
+          <input type='text' name='url_app' id='url_app' className='form-input' onChange={handleChange} />
+        </div>
+      <div className='textareaForm'>
+        <label htmlFor='description' className='message-flex'>Description</label>
+        <textarea name='description' id='description' className='form-textarea' onChange={handleChange} /><br />
+     </div>
+        <input type='submit' value='ENVOYER' className='button-send' />
+    </div>
+    {console.log(data)}
+  </form>
+  </div>
+  )
+  
+}
+
+export default NewProjectForm
--- src/components/backOffice/Projects.js
+++ src/components/backOffice/Projects.js
@@ -1,17 +1,53 @@
-import React from "react";
-import AdminNav from "./AdminNav"
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
+import AdminNav from './AdminNav'
+import NewProjectForm from './NewProjectForm'
 
 //import "./Footer.css";
 
 function Projects() {
-  return (
-    <div className="adminHome">
-     <AdminNav/>
-     <h2>Mes projets</h2>
 
+  const [projects, setProjects] = useState(null)
+  const [showNewProjectModal, setShowNewProjectModal] = useState(false)
+
+  const getProjects = () => {
+    axios.get('http://localhost:8080/api/projects').then((res) => setProjects(res.data))
+  }
+
+  const handleDelete = (e) => {
+      const answer = window.confirm('Etes vous sûr de vouloir supprimer ce projet ?')
+      console.log(e.target.id)
+      if (answer){
+        const idProject = e.target.id
+          console.log('je veux supprimer ce projet')
+          axios.delete(`http://localhost:8080/api/projects/${idProject}`)
+          .then((res) => alert('Le projet a été supprimé'))
+          
+      } else {
+        console.log('je ne veux pas supprimer ce projet !!!!')
+      }
+  }
+
+  useEffect(() => getProjects(), [])
+
+  return projects ? ( showNewProjectModal ? (
+    <div><NewProjectForm setShowNewProjectModal={setShowNewProjectModal} showNewProjectModal={showNewProjectModal}/></div>
+    ) :(
+    <div className='adminHome'>
+      <AdminNav />
+      <h2>Mes projets</h2>
+      {projects.map( project => (
+          <div key={project.id} >
+              {project.id}, {project.name}, {project.techno} 
+              <button id={project.id} onClick={handleDelete}> supprimer </button>
+          </div>
+      ))}
+      <button onClick={() => setShowNewProjectModal(true)}>ajouter un projet</button>
+      {console.log(projects)}
     </div>
-  );
+  )
+  ) : "loading"
 }
 
-export default Projects;
+export default Projects
